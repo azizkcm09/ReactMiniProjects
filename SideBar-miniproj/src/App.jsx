64aecf67bc06a946dbc8346a8ec85a2e5bb948c4
aziz <azizkacem.mk@gmail.com>
2025-09-26 00:37:57 +0100

    adding Sidebar-MiniProj

---
+++ SideBar-miniproj/src/App.jsx
@@ -0,0 +1,13 @@
+import Modal from "./Modal";
+import Sidebar from "./Sidebar";
+import Home from "./Home";
+const App = () => {
+  return (
+    <>
+      <Home />
+      <Modal />
+      <Sidebar />
+    </>
+  );
+};
+export default App;
